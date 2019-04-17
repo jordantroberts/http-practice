b69882ff9550b9c096476a93c4e021f598e21c49
Jordan <jordanroberts@talk21.com>
2019-04-17 10:03:11 +0100

    Adding in form

--- Gemfile
+++ Gemfile
@@ -2,5 +2,7 @@ source 'https://rubygems.org'
 
 ruby '2.6.0'
 
+gem 'capybara'
 gem 'shotgun'
 gem 'sinatra'
+gem 'selenium-webdriver'
--- app.rb
+++ app.rb
@@ -14,7 +14,17 @@ get '/tara' do
   "hey Tara!"
 end
 
-get '/cat' do
-  @random_name = ["Amigo", "Oscar", "Viking"].sample
-  erb(:index)
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb :index
+end
+
+get '/cat_form' do
+  erb :cat_form
+end
+
+post '/named_cat' do
+p params
+@name = params[:name]
+erb :index
 end
